5b43b9f0811d40d7fa94d59be11f5f125cb6ada2
Lupino <lmjubuntu@gmail.com>
2026-07-14 12:59:25 +0800

    refactor(web): isolate embedded asset package
    Move FileProxy.WebAssets and file-embed into a dedicated local package so the main library depends only on the compiled asset interface. Register the package with Stack and Cabal, update asset source tracking, and add complete HTML metadata plus the favicon to source and distribution assets.

--- web-assets/src/FileProxy/WebAssets.hs
+++ web-assets/src/FileProxy/WebAssets.hs
@@ -3,7 +3,7 @@
 module FileProxy.WebAssets (embeddedFiles) where
 
 import qualified Data.ByteString as BS
-import           Data.FileEmbed (embedDir)
+import           Data.FileEmbed  (embedDir)
 
 embeddedFiles :: [(FilePath, BS.ByteString)]
 embeddedFiles = $(embedDir "web/dist")
