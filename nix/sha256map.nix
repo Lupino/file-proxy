962859ed82ba8d8fb9c6122e60b5e8a7b6b40793
Lupino <lmjubuntu@gmail.com>
2026-07-14 11:29:42 +0800

    Package web assets for Nix builds
    Track the generated Vite bundle so cleanGit includes the directory required by FileProxy.WebAssets at compile time. Pin Scotty's compatible source revision and record its Nix fetch hash for reproducible cross builds.

--- nix/sha256map.nix
+++ nix/sha256map.nix
@@ -1,4 +1,5 @@
 {
-  "https://github.com/Lupino/haskell-periodic.git"."c05c7c46e8a0bec492689499641ac1a0e6be92fa" = "00w8hgywwyib05ymf59yvqbwa3zbhayhffqny4365bya0syackzb";
   "https://github.com/Lupino/metro.git"."5eeed4a66451e830e8a27026315f34a41c61a02f" = "1lnlcn4r7d7achjn1kb4ijl15fh1yyhn1xqbvwwjqa4bmk7sxyk8";
+  "https://github.com/Lupino/haskell-periodic.git"."c05c7c46e8a0bec492689499641ac1a0e6be92fa" = "00w8hgywwyib05ymf59yvqbwa3zbhayhffqny4365bya0syackzb";
+  "https://github.com/scotty-web/scotty.git"."4257621e15a0f35bd48ab4fad064ca7731430065" = "097s16rr7vmpplxvziap2ar83avaw4b4ac223dcxf7d1608z4wl0";
 }
